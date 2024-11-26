985312242f01ff1e9ef8d1e363a78fcf861bc6b9
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2024-11-26 19:22:31 +0100

    feat: Add basic config system
    Added config system and menus to make it easier to edit the config.

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,14 +1,14 @@
 import praw
 
 
-def get_post_list(instance: praw.Reddit, subreddit: str, mode: str = "hot", amount: int = 10) -> list:
-    print(f"Fetching {mode} {amount} post(s) from {subreddit}...")
-    print("Keep in mind that the NSFW posts will be removed afterwards.")
+def get_post_list(instance: praw.Reddit, subreddit: str, mode: str = "hot", time_filter: str = "all", amount: int = 10) -> list:
+    print(f"\nFetching {amount} {f'{mode} of {time_filter}' if mode == 'top' else mode} post(s) from {subreddit}...")
+    print("Keep in mind that the NSFW posts will be removed afterwards.\n")
     match mode:
         case "hot":
             return [submission for submission in instance.subreddit(subreddit).hot(limit=amount)]
         case "top":
-            return [submission for submission in instance.subreddit(subreddit).top(limit=amount)]
+            return [submission for submission in instance.subreddit(subreddit).top(time_filter=time_filter, limit=amount)]
         case "rising":
             return [submission for submission in instance.subreddit(subreddit).rising(limit=amount)]
         case "new":
--- setup/setup.py
+++ setup/setup.py
@@ -1,9 +1,49 @@
+import os
 import pathlib
+import choice
+
 from .config import get_config_from_file
 
 def setup() -> dict:
+    runnning = True
+    data = get_config_from_file()
+    selected_section = "subreddit"
+
+
+    while runnning:
+        sections = get_config_from_file("sections")
+        item, act = choice.Menu(sections, ["edit", "reset"], ["save", "discard"], title="Choose an entry and what to do with it or continue.").ask()
+        match act:
+            case "edit":
+                selected_section = item
+                keys = []
+                for _key, value in data.items(item):
+                    keys.append(_key)
+
+                key_choice = choice.Menu(keys,["edit"], ["back"], title=f"Choose a key to edit in the '{item}' section.").ask()
+                if isinstance(key_choice, tuple) and key_choice[1] == "back":
+                    continue
+                new_value = choice.Input(f"New value? Current for [{item}][{key_choice}]: {data[item][key_choice]}  **Leave blank to keep current").ask()
+                data[item][key_choice] = new_value
+                
+
+            case "save":
+                if choice.Binary("Are you sure you want to save?").ask():     
+                    with open(f"{os.getcwd()}/setup/config.ini", 'w') as configfile:
+                        if data:
+                            data.write(configfile)
+                    runnning = False
+                
+            
+            case "exit":
+                if choice.Binary("Are you sure you want to discard?").ask():
+                    data = get_config_from_file()
+                    runnning = False
+    
     create_folders()
+    
+    return dict(data.items(selected_section))
 
 def create_folders():
     pathlib.Path(f"{pathlib.Path.cwd()}/audios").mkdir(exist_ok=True)
-    pathlib.Path(f"{pathlib.Path.cwd()}/screenshots").mkdir()
+    pathlib.Path(f"{pathlib.Path.cwd()}/screenshots").mkdir(exist_ok=True)
